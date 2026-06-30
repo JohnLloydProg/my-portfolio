b40ebe39a65ac5f1922f6c1625168f58724efaf5
JohnLloydProg <johnlloydunida0@gmail.com>
2026-06-30 13:38:00 +0800

    feat(projects): migrated data to json file
    Implemented file read for project data in the projects page and individual project page. Changed layout of individual project page.

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,4 +1,14 @@
-import { PROJECTS_DATA } from "../page";
+import { promises as fs } from "node:fs";
+import path from "node:path";
+import Image from "next/image";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+import type { Project } from "../page";
 
 export default async function ProjectPage({
 	params,
@@ -6,56 +16,62 @@ export default async function ProjectPage({
 	params: Promise<{ slug: string }>;
 }) {
 	const { slug } = await params;
-	const project = PROJECTS_DATA.find((project) => project.id === slug);
+
+	const filePath = path.join(process.cwd(), "data", "projects.json");
+	const fileContent = await fs.readFile(filePath, "utf8");
+	const data: Project[] = JSON.parse(fileContent);
+	const project = data.find((project) => project.id === slug);
 	if (!project) return <h1>Not found!</h1>;
 
+	try {
+		const projectImagesPath = path.join(
+			process.cwd(),
+			"public",
+			"projects",
+			project.id,
+		);
+		const files = await fs.readdir(projectImagesPath, "utf8");
+		project.imgs = files.map((name) => `/projects/${project.id}/${name}`);
+	} catch (error) {
+		console.log(error);
+	}
+
+	await new Promise((resolve) => setTimeout(resolve, 2000));
+
 	return (
-		<div className="flex items-center justify-center">
-			<div
-				key={`detail-${project.id}`}
-				id={project.id}
-				className="min-h-lvh flex flex-col w-full max-w-6xl mt-20"
-			>
-				{project.id === "p1" && (
-					<div className="p1-custom-layout">
-						<h1 className="text-5xl font-bold mb-6 text-green-400">
-							Custom Title for P1!
-						</h1>
-						<p>{project.description}</p>
-						<div className="grid grid-cols-2 gap-4 mt-8">
-							<div className="h-40 bg-slate-800 rounded-xl">Graph 1</div>
-							<div className="h-40 bg-slate-800 rounded-xl">Graph 2</div>
-						</div>
-					</div>
+		<div className="flex justify-center">
+			<div className="min-h-lvh flex flex-col w-full max-w-6xl mt-10 mb-10">
+				{project.imgs && (
+					<Carousel
+						className=" w-full md:w-65/100 mx-auto"
+						opts={{ loop: true }}
+					>
+						<CarouselContent>
+							{project.imgs.map((image, index) => (
+								<CarouselItem key={image}>
+									<div className="relative w-full aspect-video rounded-xl overflow-clip">
+										<Image src={image} alt={`${project.name}-${index}`} fill />
+									</div>
+								</CarouselItem>
+							))}
+						</CarouselContent>
+						<CarouselPrevious
+							size="icon-lg"
+							className="rounded-full bg-obsidian-black/80 hover:bg-denim-blue text-platinum-white border-2 border-platinum-white shadow-lg backdrop-blur-sm transition-all hover:scale-110"
+						/>
+						<CarouselNext
+							size="icon-lg"
+							className="rounded-full bg-obsidian-black/80 hover:bg-denim-blue text-platinum-white border-2 border-platinum-white shadow-lg backdrop-blur-sm transition-all hover:scale-110"
+						/>
+					</Carousel>
 				)}
-
-				{project.id === "p2" && (
-					<div className="p2-custom-layout">
-						<h1 className="text-5xl font-bold mb-6 text-purple-500">
-							Anime Tracker Setup
-						</h1>
-						<p className="italic text-gray-400">
-							A completely different layout.
-						</p>
-						<div className="h-96 bg-slate-800 mt-8 rounded-full flex items-center justify-center">
-							Custom Round Graphic
-						</div>
-					</div>
-				)}
-
-				{project.id !== "p1" && project.id !== "p2" && (
-					<div>
-						<span className="text-sm font-semibold text-blue-500 tracking-wider uppercase mb-2">
-							{project.framework}
-						</span>
-						<h1 className="text-5xl font-bold text-white mb-6">
-							{project.name}
-						</h1>
-						<p className="text-xl text-gray-400 leading-relaxed mb-8">
-							{project.description}
-						</p>
-					</div>
-				)}
+				<p className="text-sm font-semibold text-denim-blue tracking-wider uppercase mb-2 mt-10">
+					{project.framework}
+				</p>
+				<div className="flex">
+					<h1 className="text-5xl font-bold font-inter mb-6">{project.name}</h1>
+				</div>
+				<p className="text-xl leading-relaxed">{project.description}</p>
 			</div>
 		</div>
 	);
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,6 @@
+import path from "path";
 import ProjectCard from "./projectCard";
+import { promises as fs } from "fs";
 
 // --- INTERFACES ---
 export interface Project {
@@ -6,6 +8,7 @@ export interface Project {
 	name: string;
 	framework: string;
 	description: string;
+	imgs?: string[];
 }
 
 // --- MOCK DATA ---
@@ -17,53 +20,21 @@ const FILTERS: string[] = [
 	"React Native",
 ];
 
-export const PROJECTS_DATA: Project[] = [
-	{
-		id: "p1",
-		name: "E-Commerce Dashboard",
-		framework: "React.js",
-		description: "Full-stack dashboard for managing store inventory.",
-	},
-	{
-		id: "p2",
-		name: "Anime Tracker",
-		framework: "Next.js",
-		description: "Track your favorite shows with real-time updates.",
-	},
-	{
-		id: "p3",
-		name: "Auth Microservice",
-		framework: "Node.js",
-		description: "Secure JWT authentication service.",
-	},
-	{
-		id: "p4",
-		name: "Payment Gateway API",
-		framework: "Node.js",
-		description: "Stripe integration for subscription processing.",
-	},
-	{
-		id: "p5",
-		name: "Fitness App",
-		framework: "React Native",
-		description: "Workout tracker and meal planner.",
-	},
-	{
-		id: "p6",
-		name: "Portfolio V2",
-		framework: "Next.js",
-		description: "Personal portfolio with interactive animations.",
-	},
-];
-
 export default async function Projects() {
+	const filePath = path.join(process.cwd(), "data", "projects.json");
+	const fileContent = await fs.readFile(filePath, "utf8");
+	const data: Project[] = JSON.parse(fileContent);
+
+	await new Promise((resolve) => setTimeout(resolve, 1000));
+
 	return (
 		<div className="flex flex-col items-center min-h-lvh">
 			<h1 className="font-inter text-5xl font-bold mt-10">
-				Explore My Projects
+				Explore <span className="text-pastel-blue">My</span>{" "}
+				<span className="text-denim-blue">Projects</span>
 			</h1>
 			<div className="flex flex-wrap gap-10 max-w-6xl justify-center mt-20">
-				{PROJECTS_DATA.map((project) => (
+				{data.map((project) => (
 					<ProjectCard key={project.id} project={project} />
 				))}
 			</div>
